e10600931bd8d7e65f5e7e912835c0b821eccc39
sjoseph1848 <sjoseph1848@gmail.com>
2021-03-30 21:58:59 -0500

    added the photo gallery section

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ var weddingAnimation = bodymovin.loadAnimation({
 weddingAnimation.play();
 
 console.log('Have it');
+
+const hamburgerContainer = document.querySelector('#header__main-nav');
+const hamburger = document.querySelector('.header__main-nav--hamburger');
+const links = document.querySelectorAll('.header__main-nav--links li');
+
+hamburger.addEventListener('click', () => {
+    hamburgerContainer.classList.toggle('clicked');
+    links.forEach((link) => {
+        link.classList.toggle('fade')
+    })
+})
